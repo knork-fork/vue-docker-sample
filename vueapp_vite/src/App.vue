966a3dfb3a494b986170550b719d257696857e18
Luka Knezic <luka@giscloud.com>
2024-04-12 18:18:24 +0200

    Add vuetify examples

--- vueapp_vite/src/App.vue
+++ vueapp_vite/src/App.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue';
+import ExampleGraph from './components/ExampleGraph.vue';
 </script>
 
 <template>
-  <div>
+  <!--<div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,7 +12,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="This is a test message." />
+  <HelloWorld msg="This is a test message." />-->
+  <ExampleGraph />
 </template>
 
 <style scoped>
--- vueapp_vite/src/plugins/vuetify.ts
+++ vueapp_vite/src/plugins/vuetify.ts
@@ -6,13 +6,15 @@ import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import { mdi } from "vuetify/iconsets/mdi";
 import { VTimePicker } from 'vuetify/labs/VTimePicker'
+import { VSparkline } from 'vuetify/labs/VSparkline'
 
 // Vuetify options
 const vuetify = createVuetify({
     blueprint: md3,
     components: {
         ...components,
-        VTimePicker
+        VTimePicker,
+        VSparkline,
     },
     directives,
     icons: {
