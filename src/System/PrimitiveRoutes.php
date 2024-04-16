cbdbbde6ff6d75707fc15b29840faacfb7d7fae7
Luka Knezic <luka@giscloud.com>
2024-04-16 20:19:03 +0200

    Setup nginx as reverse proxy
    Nginx will now forward requests either to frontend container or to backend container based on url.
    This is done so frontend can send requests to "itself", circumventing CORS issues.

--- src/System/PrimitiveRoutes.php
+++ src/System/PrimitiveRoutes.php
@@ -8,6 +8,8 @@ use App\Exception\NotFoundException;
 
 class PrimitiveRoutes
 {
+    const API_PREFIX = '/api';
+
     /**
      * @return Route[]
      */
@@ -19,8 +21,18 @@ class PrimitiveRoutes
         ];
     }
 
+    private static function stripPrefix(string $route): string
+    {
+        if (strpos($route, self::API_PREFIX) === 0) {
+            return substr($route, strlen(self::API_PREFIX));
+        } else {
+            return $route;
+        }
+    }
+
     public static function invokeRoute(string $route): void
     {
+        $route = self::stripPrefix($route);
         $routes = self::getRoutes();
         if (!isset($routes[$route])) {
             throw new NotFoundException();
