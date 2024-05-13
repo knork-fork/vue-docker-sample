d3fbe8c8758e15d31708935170bf2d97548f8d02
Luka Knezic <luka@giscloud.com>
2024-05-13 17:32:14 +0200

    Use @ addressing when importing assets

--- vueapp_vite/src/main.ts
+++ vueapp_vite/src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from 'vue';
-import './style.css';
-import App from './App.vue';
-import vuetify from "./plugins/vuetify";
+import '@/style.css';
+import '@/emoji.css';
+import App from '@/App.vue';
+import vuetify from "@/plugins/vuetify";
 import router from "@/router";
 import 'vuetify/styles';
 
