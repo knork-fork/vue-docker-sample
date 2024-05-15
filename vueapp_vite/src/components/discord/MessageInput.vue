edc24aff9b2dfa2fbef72ecd5232788716a449b8
Luka Knezic <luka@giscloud.com>
2024-05-15 17:46:55 +0200

    Add textarea to message input element

--- vueapp_vite/src/components/discord/MessageInput.vue
+++ vueapp_vite/src/components/discord/MessageInput.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import AttachFileButton from '@/components/discord/buttons/AttachFileButton.vue';
 import EmojiButton from '@/components/discord/buttons/EmojiButton.vue';
+import MessageInputTextarea from '@/components/discord/inputs/MessageInputTextarea.vue';
 </script>
 
 <template>
@@ -9,7 +10,9 @@ import EmojiButton from '@/components/discord/buttons/EmojiButton.vue';
             <div class="input-file">
                 <AttachFileButton />
             </div>
-            <div class="input-text">Message #general</div>
+            <div class="input-text">
+                <MessageInputTextarea />
+            </div>
             <div class="input-emoji">
                 <EmojiButton />
             </div>
@@ -22,20 +25,20 @@ import EmojiButton from '@/components/discord/buttons/EmojiButton.vue';
     display: flex;
     align-items: top;
     justify-content: center;
-    height: 66px;
     min-height: 66px;
+    flex-shrink: 0;
 }
 
 .message-input {
     display: flex;
-    align-items: center;
+    align-items: top;
     justify-content: center;
     background-color: #373a40;
-    color: #696b74;
-    height: 44px;
+    min-height: 44px;
     width: 100%;
     margin-left: 16px;
     margin-right: 16px;
+    margin-bottom: 24px;
     border-radius: 8px;
 }
 
@@ -43,101 +46,25 @@ import EmojiButton from '@/components/discord/buttons/EmojiButton.vue';
     display: flex;
     align-items: center;
     justify-content: center;
-    /*background-color: red;*/
     width: 56px;
     min-width: 56px;
-    height: 100%;
+    height: 44px;
 }
 
 .input-text {
     display: flex;
-    align-items: center;
+    align-items: top;
     justify-content: left;
-    /*background-color: white;*/
     flex-grow: 1;
-    height: 100%;
+    min-height: 44px;
 }
 
 .input-emoji {
     display: flex;
     align-items: center;
     justify-content: center;
-    /*background-color: red;*/
     width: 56px;
     min-width: 56px;
-    height: 100%;
+    height: 44px;
 }
 </style>
-
-<!--<template>
-    <v-form>
-      <v-container>
-        <v-row>
-          <v-col cols="12">
-            <v-text-field
-              v-model="message"
-              :append-icon="message ? 'mdi-send' : 'mdi-microphone'"
-              :append-inner-icon="marker ? 'mdi-map-marker' : 'mdi-map-marker-off'"
-              :prepend-icon="icon"
-              clear-icon="mdi-close-circle"
-              type="text"
-              variant="filled"
-              clearable
-              @click:append="sendMessage"
-              @click:append-inner="toggleMarker"
-              @click:clear="clearMessage"
-              @click:prepend="changeIcon"
-              placeholder="Message <channel name>"
-            ></v-text-field>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-form>
-  </template>
-  <script>
-    export default {
-      data: () => ({
-        message: '',
-        marker: true,
-        iconIndex: 0,
-        icons: [
-          'mdi-emoticon',
-          'mdi-emoticon-cool',
-          'mdi-emoticon-dead',
-          'mdi-emoticon-excited',
-          'mdi-emoticon-happy',
-          'mdi-emoticon-neutral',
-          'mdi-emoticon-sad',
-          'mdi-emoticon-tongue',
-        ],
-      }),
-  
-      computed: {
-        icon () {
-          return this.icons[this.iconIndex]
-        },
-      },
-  
-      methods: {
-        toggleMarker () {
-          this.marker = !this.marker
-        },
-        sendMessage () {
-          this.resetIcon()
-          this.clearMessage()
-        },
-        clearMessage () {
-          this.message = ''
-        },
-        resetIcon () {
-          this.iconIndex = 0
-        },
-        changeIcon () {
-          this.iconIndex === this.icons.length - 1
-            ? this.iconIndex = 0
-            : this.iconIndex++
-        },
-      },
-    }
-  </script>
--->
---
+++ vueapp_vite/src/components/discord/inputs/MessageInputTextarea.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+const textareaDefaultHeight = '44px';
+const textareaRef = ref<HTMLTextAreaElement | null>(null);
+
+onMounted(() => {
+    if (textareaRef.value) {
+        textareaRef.value.style.height = textareaDefaultHeight;
+    }
+});
+
+function resizeTextarea(): void {
+    if (textareaRef.value) {
+        textareaRef.value.style.height = textareaDefaultHeight;
+        textareaRef.value.style.height = `${textareaRef.value.scrollHeight}px`;
+    }
+}
+</script>
+
+<template>
+    <textarea
+        ref="textareaRef"
+        placeholder="Message #general"
+        @input="resizeTextarea()"
+    />
+</template>
+
+<style scoped>
+textarea {
+    border: none;
+    outline: none;
+    color: white;
+    font-size: 16px;
+    width: 100%;
+    height: 100%;
+    resize: none;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    max-height: 500px;
+}
+
+textarea::placeholder {
+    color: #696b74;
+}
+</style>
